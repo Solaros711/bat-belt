038d96eda320048ed0f4ba97d0459a4fa42c01e2
Solaros711 <spurdy885@gmail.com>
2020-07-28 20:02:56 -0700

    test

--- main.js
+++ main.js
@@ -1,29 +1,71 @@
 const _ = {}
 
 _.each = (array, iteratee) => {
-  // your code here
+  var new_array = new Array(array.length)
+
+  for(let i = 0; i < array.length; i++){
+    new_array[i] = iteratee(array[i], i, array)
+  }
+  return new_array
 }
 
 _.map = (array, iteratee) => {
-  // your code here
+  var new_array = new Array(array.length)
+
+  for(let i=0; i < array.length; i++){
+    new_array[i] = iteratee(array[i], i, array)
+  }
+
+  return new_array
 }
 
 _.filter = (array, predicate) => {
-  // your code here
+  var new_array = []
+
+  for(let i = 0; i < array.length; i++){
+    if(predicate(array[i], i, array)){
+      console.log()
+      new_array.push(array[i])
+    }
+  }
+  return new_array
 }
 
 
 _.find = (array, predicate) => {
-  // your code here
+  var value = undefined
+
+  for(let i = 0; i < array.length; i++){
+    if(predicate(array[i], i, array)){
+      value = array[i]
+      break
+    }
+  }
+
+  return value
 }
 
 
-_.random = (min, max) => {
-  // your code here
+_.random = (min=0, max) => {
+  var rand
+
+  rand = Math.floor(Math.random() * Math.floor(Math.abs(min) + Math.abs(max)))
+  
+  return rand - Math.abs(min)
 }
 
 _.range = (start, stop, step) => {
-  // your code here
+  var list = []
+  for(let i=start; i<stop; i++){
+    if(step){
+      if(i % step == 0){
+        list.push(i)
+      }
+    }else{
+      list.push(i)
+    }
+  }
+  return list
 }
 
 
